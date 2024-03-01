34b236fd53293e93e7cd456cb8ad06102dee9541
Anon <84020006+anonthedev@users.noreply.github.com>
2024-03-01 12:08:00 +0530

    handled space in between query edge case

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -4,6 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest, res: NextResponse){
     const filter = req.nextUrl.searchParams.get("filter")!;
     const query = req.nextUrl.searchParams.get("query")!;
-    const resp = await axios.get(`https://librivox.org/api/feed/audiobooks/${filter}/%5E${query}`)
+    const resp = await axios.get(`https://librivox.org/api/feed/audiobooks/${filter}/%5E${query.split(" ")[0]}`)
     return NextResponse.json(resp.data)
 }
