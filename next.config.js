5e551849a02c02c86614388df13d5a2809bed215
Anon <opss59332@gmail.com>
2024-03-02 19:59:12 +0530

    PWA stuff

--- next.config.js
+++ next.config.js
@@ -19,8 +19,8 @@ const nextConfig = {
 const withSerwist = require("@serwist/next").default({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
+  swSrc: "src/app/sw.ts",
+  swDest: "src/public/sw.js",
 });
 
 
