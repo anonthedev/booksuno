8f438b5063570157a5ae0bef2d787f8a2129ec4c
Anon <opss59332@gmail.com>
2024-03-05 19:52:31 +0530

    change donate button text

--- src/Components/DonateButton.tsx
+++ src/Components/DonateButton.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function DonateButton(){
   return (
     <Link className='bg-yellow-500 py-2 px-4 rounded-md text-black font-bold font-raleway' target='_blank' href={"/support"}>
-        Buy me a thing!
+        Buy me a drink!
     </Link>
   )
 };
